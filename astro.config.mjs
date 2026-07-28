3750be3e8083f63a85f99d57643919c00bff3e5d
bpn <bipinlamsal2004@gmail.com>
2026-07-28 22:08:21 +0530

    toolhub.ext.io | thanks freedns.afraid.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/ToolHub/",
+  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
